1de24adaa170e162e82d5f82c5a07de7fd2b0674
vishnu1-git <kanukuvishnu1@gmail.com>
2026-04-26 11:00:47 +0530

    file size limit decreased

--- backend/app.py
+++ backend/app.py
@@ -24,7 +24,7 @@ DB_PATH       = os.path.join(BASE_DIR, "intelli_credit.db")
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 
 ALLOWED_EXTENSIONS = {"pdf"}
-MAX_FILE_MB        = 20
+MAX_FILE_MB        = 5
 
 def get_db():
     conn = sqlite3.connect(DB_PATH)
